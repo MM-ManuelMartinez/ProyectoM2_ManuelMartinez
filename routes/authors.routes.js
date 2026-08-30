42c86bf54757d2091da7f8789197708764bf8699
Manuel Martinez <martinezzmanuel515@gmail.com>
2026-08-30 22:12:41 +0000

    feat: agregar listado de authors

--- routes/authors.routes.js
+++ routes/authors.routes.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const routerAuthors = express.Router();
 
+const {obtenerAuthors} = require("../controllers/authors.controllers.js");
+
+routerAuthors.get("/", obtenerAuthors);
+
 module.exports = routerAuthors;
